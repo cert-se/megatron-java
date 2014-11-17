d09e551fc4e3eee546dbaa234ec481f2f7547583
gorpe <gorpe@users.noreply.github.com>
2014-11-17 10:20:39 +0100

    TicketHandler functionality added

---
+++ src/se/sitic/megatron/tickethandler/ITicketHandler.java
@@ -0,0 +1,22 @@
+package se.sitic.megatron.tickethandler;
+
+
+import java.util.Map;
+
+import se.sitic.megatron.core.MegatronException;
+
+public interface ITicketHandler {
+    
+    public void init() throws MegatronException;
+    
+    /**
+     * The method getNewTicketId returns a new ticket-ID from the ticketing 
+     * system. It takes a key/value map with the necessary input parameters. 
+     * 
+     * @param values
+     * @return
+     */
+    
+    public String getNewTicketId(Map<String,String> values);
+
+}
